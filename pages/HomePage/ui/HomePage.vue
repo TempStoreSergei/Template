da489e80617af88f9aef5277f59ec9c044237850
sergej <sergawork@gmail.com>
2024-08-28 10:56:11 +0300

    feat: Pwa

--- pages/HomePage/ui/HomePage.vue
+++ pages/HomePage/ui/HomePage.vue
@@ -1,24 +1,36 @@
 <template>
-	<div class="wrapper">
-		<div class="container">
+  <div class="wrapper">
+    <div class="container">
       <h1 class="title">
-        Hello from  <a class="link" target="_blank" href="https://github.com/yunglocokid">@yunglocokid</a>
+        Hello from
+        <a class="link" target="_blank" href="https://github.com/yunglocokid"
+          >@yunglocokid</a
+        >
       </h1>
-          <button class="button" @click="increment">Count is: {{count}}</button>
-		</div>
-	</div>
+      <button class="button" @click="increment">Count is: {{ count }}</button>
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+const count = ref(1);
+
+const increment = () => {
+  count.value += 1;
+};
+</script>
 
 <style>
 body {
-  cursor: url("https://vsthemes.org/uploads/cursors/24897/ec9c27ec4afcbc9e321f3114ee514696.webp"),auto;
+  cursor: url("https://vsthemes.org/uploads/cursors/24897/ec9c27ec4afcbc9e321f3114ee514696.webp"),
+    auto;
 }
 @keyframes cursor-blink {
   0% {
     opacity: 0;
   }
 }
-.container{
+.container {
   color: aliceblue;
   box-sizing: border-box;
   flex-flow: column wrap;
@@ -28,13 +40,13 @@ body {
   align-items: center;
   height: 100vh;
 }
-.link{
+.link {
   background: linear-gradient(45deg, #9c20aa 33%, #fb3570);
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
-  color: #0B2349;
+  color: #0b2349;
 }
-.link::after{
+.link::after {
   content: "";
   width: 5px;
   height: 25px;
@@ -46,16 +58,17 @@ body {
   text-decoration: none;
   color: blueviolet;
 }
-.wrapper{
+.wrapper {
   background: black;
   font-family: "Dubai Medium";
 }
 
-.button{
+.button {
   cursor: pointer;
   background:
-      linear-gradient(#000 0 0) padding-box, /*this is your grey background*/
-      linear-gradient(to right, #9c20aa, #fb3570) border-box;
+    linear-gradient(#000 0 0) padding-box,
+    /*this is your grey background*/ linear-gradient(to right, #9c20aa, #fb3570)
+      border-box;
   color: aliceblue;
   padding: 10px;
   border: 5px solid transparent;
@@ -63,11 +76,3 @@ body {
   display: inline-block;
 }
 </style>
-
-<script setup lang="ts">
-const count = ref(1)
-
-const increment = () => {
-  count.value += 1
-}
-</script>
