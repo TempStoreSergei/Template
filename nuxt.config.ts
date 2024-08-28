21a300f1e4a48761f0169f68a8c63d37d691f75a
sergej <sergawork@gmail.com>
2024-08-28 09:44:01 +0300

    feat: improvment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,27 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	devtools: { enabled: true },
-	modules: [],
-	css: ["@/app/normalize.min.css","@/app/global.css"],
+								devtools: { enabled: true },
+								modules: ["@vite-pwa/nuxt", "@pinia/nuxt", "@ant-design-vue/nuxt", '@ant-design-vue/nuxt'],
+								css: ["@/app/normalize.min.css","@/app/global.css"],
 
-	imports: {
-		dirs: [
-			"shared/composables",
-			"shared/utils",
-			// You can specify here any folder from which you want to auto-import
-		],
-	},
+								imports: {
+																dirs: [
+																								"shared/composables",
+																								"shared/utils",
+																								// You can specify here any folder from which you want to auto-import
+																],
+								},
 
-	dir: {
-		pages: "routes",
-	},
+								dir: {
+																pages: "routes",
+								},
 
-	components: {
-		dirs: [
-			{
-				path: "~/shared/components",
-				pathPrefix: false,
-			},
-		],
-	},
+								components: {
+																dirs: [
+																								{
+																																path: "~/shared/components",
+																																pathPrefix: false,
+																								},
+																],
+								},
 })
